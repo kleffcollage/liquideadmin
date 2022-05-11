68db65368e2474b9b815a21a4812ced355a53e5e
adelowomi <40185920+adelowomi@users.noreply.github.com>
2022-05-11 11:41:10 +0100

    added docker and changed action workflow

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "781eeda57cfc139c"; }
+/******/ 		__webpack_require__.h = function() { return "6bd34ae2176b8091"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
