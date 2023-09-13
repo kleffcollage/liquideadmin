b52a97e81e6c2c5398f57a4b9f317aa8419892c8
adelowomi <adelowomi@gmail.com>
2023-09-13 11:08:24 +0100

    fixed imports

--- src/Services/models/TransactionViewStandardResponse.ts
+++ src/Services/models/TransactionViewStandardResponse.ts
@@ -2,17 +2,19 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import type { TransactionView } from './TransactionView';
+import { TransactionView } from "types/api/transaction-view";
+
+// import type { TransactionView } from "types/api/transaction-view";
 
 export type TransactionViewStandardResponse = {
-    href?: string | null;
-    relations?: Array<string> | null;
-    method?: string | null;
-    routeName?: string | null;
-    routeValues?: any;
-    status?: boolean;
-    message?: string | null;
-    data?: TransactionView;
-    statusCode?: string | null;
-    errors?: any;
+  href?: string | null;
+  relations?: Array<string> | null;
+  method?: string | null;
+  routeName?: string | null;
+  routeValues?: any;
+  status?: boolean;
+  message?: string | null;
+  data?: TransactionView;
+  statusCode?: string | null;
+  errors?: any;
 };
